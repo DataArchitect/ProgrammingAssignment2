1a872dde0d1e732d0eb8686165c9282792a4ee18
DataArchitect <dbatothestars@aol.com>
2015-07-16 08:03:27 -0400

    First pass at the functions
    Looks good on 2x2 with and without cache
    > myinv <- cacheSolve(i); myinv; m
    getting cached data
         [,1] [,2]
    [1,]   -2    3
    [2,]    3   -4
         [,1] [,2]
    [1,]    4    3
    [2,]    3    2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caches inverse of matrix to avoid unnecessary recalculation
 
-## Write a short comment describing this function
+## makeCacheMatrix accepts a matrix parameter and calculates
+## stores the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) inv <<- inv
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+    
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the matrix either from 
+## calculation or cache (if available)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    # Have we cached this already?
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    # No cache hit, so solve the matrix
+    data <- x$get()
+    inv <- solve(data, ...)
+    
+    # Store the result in cache
+    x$setinv(inv)
+    
+    # Return result on calculated code path
+    inv
 }
